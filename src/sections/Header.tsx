f3e908260f12b8db7ed5cce3b9a05b54ee174d60
NatnaelDemelash <natidemelash074@gmail.com>
2025-01-01 21:47:36 +0300

    add a translate/rotate animation for  hamburger menu

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Button from "@/components/Button";
-import { FC } from "react";
+import { motion, useAnimate } from "motion/react";
+import { FC, useEffect, useState } from "react";
 
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 const navItems = [
@@ -26,6 +29,79 @@ const navItems = [
 ];
 
 const Header: FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [topLineScope, topLineAnimate] = useAnimate();
+  const [bottomLineScope, bottomLineAnimate] = useAnimate();
+
+  useEffect(() => {
+    if (isOpen) {
+      topLineAnimate([
+        [
+          topLineScope.current,
+          {
+            translateY: 4,
+          },
+        ],
+        [
+          topLineScope.current,
+          {
+            rotate: 45,
+          },
+        ],
+      ]);
+
+      bottomLineAnimate([
+        [
+          bottomLineScope.current,
+          {
+            translateY: -4,
+          },
+        ],
+        [
+          bottomLineScope.current,
+          {
+            rotate: -45,
+          },
+        ],
+      ]);
+    } else {
+      topLineAnimate([
+        [
+          topLineScope.current,
+          {
+            rotate: 0,
+          },
+        ],
+        [
+          topLineScope.current,
+          {
+            translateY: 0,
+          },
+        ],
+      ]);
+      bottomLineAnimate([
+        [
+          bottomLineScope.current,
+          {
+            rotate: 0,
+          },
+        ],
+        [
+          bottomLineScope.current,
+          {
+            translateY: 0,
+          },
+        ],
+      ]);
+    }
+  }, [
+    isOpen,
+    topLineAnimate,
+    topLineScope,
+    bottomLineAnimate,
+    bottomLineScope,
+  ]);
+
   return (
     <header>
       <div className="fixed top-0 left-0 w-full backdrop-blur-md mix-blend-difference">
@@ -46,7 +122,10 @@ const Header: FC = () => {
         <div className="container !max-w-full">
           <div className="flex justify-end items-center h-20 px-8 md:px-12 lg:px-24">
             <div className="flex items-center gap-4">
-              <div className="inline-flex items-center justify-center border border-stone-400 rounded-full size-11 bg-stone-200">
+              <div
+                className="inline-flex items-center justify-center border border-stone-400 rounded-full size-11 bg-stone-200"
+                onClick={() => setIsOpen(!isOpen)}
+              >
                 <svg
                   width="24"
                   height="24"
@@ -54,14 +133,30 @@ const Header: FC = () => {
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
                 >
-                  <rect
+                  <motion.rect
+                    x="3"
+                    y="7"
+                    width="18"
+                    height="2"
+                    fill="currentColor"
+                    ref={topLineScope}
+                    style={{
+                      transformOrigin: "12px 8px",
+                      // transform: "translateY(4px) rotate(45deg)",
+                    }}
+                  />
+                  <motion.rect
                     x="3"
                     y="15"
                     width="18"
                     height="2"
                     fill="currentColor"
+                    ref={bottomLineScope}
+                    style={{
+                      transformOrigin: "12px 16px",
+                      // transform: "translateY(-4px) rotate(-45deg)",
+                    }}
                   />
-                  <rect x="3" y="7" width="18" height="2" fill="currentColor" />
                 </svg>
               </div>
               <Button variant="primary" className="hidden md:block">
