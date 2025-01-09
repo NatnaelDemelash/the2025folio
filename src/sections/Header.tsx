6ec2a654a8ae8461cb8f8fa12aebfe3267355f6c
NatnaelDemelash <natidemelash074@gmail.com>
2025-01-09 21:04:09 +0300

    tooltip feature to show nav links label name

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -16,36 +16,63 @@ const Header: FC = () => {
               </span>
             </a>
 
-            {/* Right: Icon Navigation Links */}
-            <nav className="flex gap-6  px-4 py-2 rounded">
-              <a
-                href="/"
-                className="flex items-center justify-center w-10 h-10 text-white transition"
-                aria-label="Tech Stacks"
-              >
-                <User size={25} />
-              </a>
-              <a
-                href="#stacks"
-                className="flex items-center justify-center w-10 h-10 text-white transition"
-                aria-label="About"
-              >
-                <Bolt size={25} />
-              </a>
-              <a
-                href="#projects"
-                className="flex items-center justify-center w-10 h-10 text-white transition"
-                aria-label="Projects"
-              >
-                <Projector size={25} />
-              </a>
-              <a
-                href="#contact"
-                className="flex items-center justify-center w-10 h-10 text-white transition"
-                aria-label="Contact"
-              >
-                <Cable size={25} />
-              </a>
+            {/* Right: Icon Navigation Links with Tooltips */}
+            <nav className="flex gap-6 px-4 py-2 rounded">
+              {/* About */}
+              <div className="relative group">
+                <a
+                  href="/"
+                  className="flex items-center justify-center w-10 h-10 text-white transition"
+                  aria-label="Tech Stacks"
+                >
+                  <User size={25} />
+                </a>
+                <div className="absolute -bottom-14 left-1/2 transform -translate-x-1/2 whitespace-nowrap px-3 py-1 text-sm text-white bg-gray-900 rounded-md opacity-0 group-hover:opacity-100 transition-opacity">
+                  About
+                </div>
+              </div>
+
+              {/* Tech Stacks */}
+              <div className="relative group">
+                <a
+                  href="#stacks"
+                  className="flex items-center justify-center w-10 h-10 text-white transition"
+                  aria-label="About"
+                >
+                  <Bolt size={25} />
+                </a>
+                <div className="absolute -bottom-14 left-1/2 transform -translate-x-1/2 whitespace-nowrap px-3 py-1 text-sm text-white bg-gray-900 rounded-md opacity-0 group-hover:opacity-100 transition-opacity">
+                  Tech Stacks
+                </div>
+              </div>
+
+              {/* Projects */}
+              <div className="relative group">
+                <a
+                  href="#projects"
+                  className="flex items-center justify-center w-10 h-10 text-white transition"
+                  aria-label="Projects"
+                >
+                  <Projector size={25} />
+                </a>
+                <div className="absolute -bottom-14 left-1/2 transform -translate-x-1/2 whitespace-nowrap px-3 py-1 text-sm text-white bg-gray-900 rounded-md opacity-0 group-hover:opacity-100 transition-opacity">
+                  Projects
+                </div>
+              </div>
+
+              {/* Contact */}
+              <div className="relative group">
+                <a
+                  href="#contact"
+                  className="flex items-center justify-center w-10 h-10 text-white transition"
+                  aria-label="Contact"
+                >
+                  <Cable size={25} />
+                </a>
+                <div className="absolute -bottom-14 left-1/2 transform -translate-x-1/2 whitespace-nowrap px-3 py-1 text-sm text-white bg-gray-900 rounded-md opacity-0 group-hover:opacity-100 transition-opacity">
+                  Contact
+                </div>
+              </div>
             </nav>
           </div>
         </div>
