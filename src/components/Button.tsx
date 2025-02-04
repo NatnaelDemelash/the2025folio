3b1ae2133b046e6d88497f9f626b8a4def03f4b4
NatnaelDemelash <natidemelash074@gmail.com>
2025-02-04 22:10:28 +0300

    font size and padding update for 100% zoom

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -14,7 +14,7 @@ const Button = (props: ButtonProps) => {
   const { children, className, variant, iconAfter, href, ...rest } = props;
 
   const baseClasses = twMerge(
-    "h-10 px-6 rounded-xl uppercase inline-flex items-center gap-2 sm:h-12 sm:px-8",
+    "h-10 px-6 rounded-xl text-sm inline-flex items-center gap-2 sm:h-11 sm:px-8 ",
     variant === "primary" && "bg-stone-800 hover:bg-stone-900 text-white",
     variant === "secondary" && "border border-orange-500 lowercase",
     variant === "text" && "h-auto px-0",
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -9,7 +9,7 @@ const Hero: FC = () => {
   return (
     <section className="flex flex-col min-h-screen items-center justify-center mt-10 text-center px-4 sm:px-6 md:px-8">
       {/* Avatar */}
-      <div className="w-24 h-24 sm:w-36 sm:h-36 rounded-full overflow-hidden shadow-md mb-6">
+      <div className="w-24 h-24 sm:w-36 sm:h-36 rounded-full overflow-hidden shadow-md mb-6 mt-3">
         <Image
           src={profile}
           alt="Avatar"
@@ -26,12 +26,12 @@ const Hero: FC = () => {
       </h1>
 
       {/* Main Heading */}
-      <h2 className="text-center max-w-3xl sm:max-w-4xl px-4 sm:px-8 md:px-12 text-2xl sm:text-4xl md:text-6xl font-medium text-gray-900  sm:leading-normal mb-4 capitalize">
+      <h2 className="text-center max-w-3xl sm:max-w-4xl px-4 sm:px-8 md:px-12 text-2xl sm:text-4xl md:text-5xl font-medium text-gray-900  sm:leading-normal mb-4 capitalize">
         Building modern web applications with creativity and precision.
       </h2>
 
       {/* Subtitle */}
-      <p className=" text-sm sm:text-base md:text-lg text-gray-600 mb-8 max-w-md sm:max-w-lg md:max-w-xl">
+      <p className=" text-sm sm:text-base md:text-[1.1rem] text-gray-600 mb-8 max-w-md sm:max-w-lg md:max-w-xl">
         a <span className="font-semibold">Front End Developer</span> passionate
         about creating beautiful, responsive, and user-friendly interfaces.
         <br /> I specialize in React, Tailwind CSS, and building seamless web
